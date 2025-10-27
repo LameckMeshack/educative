682bf685fc52ba4eda9d99ed29e2b691bc64312f
LameckMeshack <otienolameck327@gmail.com>
2025-10-27 23:42:07 +0300

    Add supervisor stash for state and chnaged strategy

--- sequence/lib/sequence/application.ex
+++ sequence/lib/sequence/application.ex
@@ -8,13 +8,13 @@ defmodule Sequence.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Sequence.Worker.start_link(arg)
-      # {Sequence.Worker, arg}
+      {Sequence.Stash, 123},
+      {Sequence.Server, nil}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Sequence.Supervisor]
+    opts = [strategy: :rest_for_one, name: Sequence.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
