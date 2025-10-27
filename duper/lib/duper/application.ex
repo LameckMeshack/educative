f90d580e654a9f96aa94b68b250b8a176c709456
LameckMeshack <otienolameck327@gmail.com>
2025-10-28 00:27:42 +0300

    Finished duper

--- duper/lib/duper/application.ex
+++ duper/lib/duper/application.ex
@@ -1,20 +1,17 @@
 defmodule Duper.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
-  @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Duper.Worker.start_link(arg)
-      # {Duper.Worker, arg}
+        Duper.Results,
+      { Duper.PathFinder,       "/usercode/duper/lib/duper/User/Pictures" },
+        Duper.WorkerSupervisor,
+      { Duper.Gatherer,         2 },
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
